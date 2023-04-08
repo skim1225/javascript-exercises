47899dcfb09a1442f86cef46dcce1d2bde0fb1e2
Sooji Kim <trollpotato12@gmail.com>
2023-04-08 11:44:24 -0700

    Complete ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        let myStr = "";
+        for (let i = 0; i < num; i++) {
+            myStr += str;
+        }
+        return myStr;
+    }
+    
 };
 
 // Do not edit below this line
