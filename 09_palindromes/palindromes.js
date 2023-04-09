f9793ac652510b5e0ee7fb6a486b15d0c2803b6b
Sooji Kim <trollpotato12@gmail.com>
2023-04-09 14:39:23 -0700

    Complete ex 9: palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    let newStr = str.replace(/\W/g, "").toLowerCase();
+    let isPal = true;
+    for (let i = 0; i < newStr.length; i++) {
+        if (newStr[i] !== newStr[newStr.length - i - 1]) {
+            isPal = false;
+        }
 
+    }
+    return isPal;
 };
 
 // Do not edit below this line
