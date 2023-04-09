a5fca88bd96bc1dec84f1a6e92c7e63598a4aef9
Sooji Kim <trollpotato12@gmail.com>
2023-04-09 13:28:17 -0700

    Complete ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArr, ...myArgs) {
+    for (let i = 0; i < myArgs.length; i++) {
+        for (let j = 0; j < myArr.length; j++) {
+            if (myArr[j] === myArgs[i]) {
+                myArr.splice(j, 1);
+                i--;
+                
+            }
+        }
+    }
+    return myArr;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
