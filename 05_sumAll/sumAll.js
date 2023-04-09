882665b758e7a57ab111709f020daa8ea1ad70ae
Sooji Kim <trollpotato12@gmail.com>
2023-04-09 13:36:35 -0700

    Complete ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    if (first < 0 || last < 0) {
+        return "ERROR";
+    }
+    if (typeof(first) !== "number" || typeof(last) !== "number") {
+        return "ERROR";
+    }
+    let small;
+    let big;
+    if (first < last) {
+        small = first;
+        big = last;
+    } else {
+        small = last;
+        big = first;
+    }
+    let total = 0;
+    for (let i = small; i <= big; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
