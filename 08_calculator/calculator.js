319e12bfb7d14be04a179c49a1c29df817c1a5c8
Sooji Kim <trollpotato12@gmail.com>
2023-04-09 14:23:40 -0700

    Complete e 8: calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = 0;
+  nums.forEach(n => {
+    total += n;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let total = 1;
+  nums.forEach(n => {
+    total *= n;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let total = 1;
+  for(let i = n; i > 1; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
