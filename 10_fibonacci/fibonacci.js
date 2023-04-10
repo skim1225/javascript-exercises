b32f21d97ba596c22209e85f3edc55017f9cc0a6
Sooji Kim <trollpotato12@gmail.com>
2023-04-10 10:05:56 -0700

    Complete ex 10: fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    let fibs = [];
+    n = parseInt(n);
+    for (let i = 0; i < n; i++) {
+        if (i === 0 || i === 1) {
+            fibs[i] = 1;
+        } else {
+            fibs[i] = fibs[i-1] + fibs[i-2];
+        }
+        console.log(fibs);
+    }
+    return fibs[n - 1];
 };
 
 // Do not edit below this line
